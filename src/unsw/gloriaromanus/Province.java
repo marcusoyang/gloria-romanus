421d9eded4c40fe53a5d3f435164dc55c3f50983
Julie <julie.seo.2000@gmail.com>
2020-10-30 04:13:42 +1100

    Created a Province class with useful metadata and getArmyStrength() method that returns the army strength of a province.

---
+++ src/unsw/gloriaromanus/Province.java
@@ -0,0 +1,28 @@
+package unsw.gloriaromanus;
+
+import java.util.ArrayList;
+
+public class Province {
+    private String name;
+    private Faction faction;
+    private ArrayList<Unit> units;
+
+    public Province(String name, Faction faction) {
+        this.name = name;
+        this.faction = faction;
+        units = new ArrayList<Unit>();
+    }
+
+    public float getArmyStrength() {
+        // the sum of number of soldiers in unit x attack x defense for all units in the army
+        int noOfSoldiers = units.size();
+        int totalAttack = 0;
+        int totalDefense = 0;
+        for (Unit u : units) {
+            totalAttack += u.getAttack();
+            totalDefense += u.getDefense();
+        }
+        
+        return (noOfSoldiers * totalAttack * totalDefense);
+    }
+}
