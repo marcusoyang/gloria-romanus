11787bc229fe65061203484e2395117beb96b4bf
Julie <julie.seo.2000@gmail.com>
2020-10-31 15:49:06 +1100

    Change calculate army strength as the simple version of what's in the program for now. It will be changed when units are implemented.

--- src/unsw/gloriaromanus/Province.java
+++ src/unsw/gloriaromanus/Province.java
@@ -6,6 +6,7 @@ public class Province {
     private String name;
     private Faction faction;
     private ArrayList<Unit> units;
+    // the below armySize variable will be deleted when units list is implemented
     private int armySize;
 
     public Province(String name, Faction faction) {
@@ -15,17 +16,19 @@ public class Province {
         this.armySize = 0;
     }
 
-    public float getArmyStrength() {
+    public int getArmyStrength() {
         // the sum of number of soldiers in unit x attack x defense for all units in the army
-        int noOfSoldiers = units.size();
-        int totalAttack = 0;
+
+        // the below code will be used when units list is implemented
+        /*int totalAttack = 0;
         int totalDefense = 0;
         for (Unit u : units) {
             totalAttack += u.getAttack();
             totalDefense += u.getDefense();
-        }
+        }*/
         
-        return (noOfSoldiers * totalAttack * totalDefense);
+        // We initially assume that a unit has 1 attack and 1 defense
+        return (armySize * armySize * armySize);
     }
 
     public String getName() {
