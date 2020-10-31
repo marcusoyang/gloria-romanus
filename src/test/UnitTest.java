e3235d9fabf392112cbc5d928c332242fdaec386
Marcus O'Yang <m.oyang@student.unsw.edu.au>
2020-10-31 02:24:08 -0700

    unit config complete

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -2,22 +2,31 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import unsw.gloriaromanus.*;
 
-public class UnitTest{
+public class UnitTest {
     @Test
-    public void blahTest(){
+    public void blahTest() {
         assertEquals("a", "a");
     }
-    
+
     @Test
-    public void blahTest2(){
+    public void blahTest2() throws IOException {
         Unit u = new Unit("legionary", 50);
         assertEquals(u.getNumTroops(), 50);
     }
+
+    @Test
+    public void checkDirectory() {
+        System.out.println(new File(".").getAbsolutePath());
+    }
+
 }
 
--- src/unsw/gloriaromanus/Unit.java
+++ src/unsw/gloriaromanus/Unit.java
@@ -1,5 +1,6 @@
 package unsw.gloriaromanus;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,43 +21,61 @@ import org.json.JSONObject;
  * armour and morale)
  */
 public class Unit {
-    private int numTroops; // the number of troops in this unit (should reduce based on depletion)
-    private int range; // range of the unit
-    private int armour; // armour defense
-    private int morale; // resistance to fleeing
-    private int speed; // ability to disengage from disadvantageous battle
-    private int attack; // can be either missile or melee attack to simplify. Could improve
-                        // implementation by differentiating!
-    private int defenseSkill; // skill to defend in battle. Does not protect from arrows!
+    private int numTroops;     // the number of troops in this unit (should reduce based on depletion)
+    private int meleeAttack;   // can be either missile or melee attack to simplify. Could improve
+    private int rangedAttack;  // implementation by differentiating!
+    private int defenseSkill;  // skill to defend in battle. Does not protect from arrows!
+    private int armour;        // armour defense
     private int shieldDefense; // a shield
+    private int morale;        // resistance to fleeing
+    private int speed;         // ability to disengage from disadvantageous battle
+    private String range;      // range of the unit
+    private String type;
+    private String ability;
+    private int turnsToProduce;
 
     public Unit(String unitType, int numTroops2) throws IOException {
-        
-        // TODO = obtain these values from the file for the unit
+
         String content = Files.readString(Paths.get("src/unsw/gloriaromanus/unit_config.json"));
         JSONObject config = new JSONObject(content);
         JSONObject unitStats = config.getJSONObject(unitType);
 
-        FileWriter file = new FileWriter("src/unsw/gloriaromanus/testFile.json");
-        file.write(unitStats.toString());
-        file.close();
-        
+        // Unit stat values obtained from unit config file
+        numTroops = numTroops2;
+        meleeAttack = unitStats.getInt("meleeAttack");
+        rangedAttack = unitStats.optInt("rangedAttack");
+        defenseSkill = unitStats.getInt("defense");
+        armour = unitStats.getInt("armour");
+        shieldDefense = unitStats.getInt("shield");
+        morale = unitStats.getInt("morale");
+        speed = unitStats.getInt("speed");
+        range = unitStats.getString("range");
+        type = unitStats.getString("type");
+        ability = unitStats.getString("ability");
+        turnsToProduce = unitStats.getInt("turnsToProduce");
+
         // TODO = shield charge ability
         // TODO = heroic charge ability
         
-        numTroops = numTroops2;
-        range = 1;
-        armour = 5;
-        morale = 10;
-        speed = 10;
-        attack = 6;
-        defenseSkill = 10;
-        shieldDefense = 3;
+        
+        
+        
     }
 
-    public int getNumTroops(){
+    public int getNumTroops() {
         return numTroops;
     }
 
-    
+    /* public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException {
+        String content = Files.readString(Paths.get("src/unsw/gloriaromanus/unit_config.json"));
+        JSONObject config = new JSONObject(content);
+        JSONObject unitStats = config.getJSONObject("berserker");
+        
+        // ObjectMapper objectMapper = new ObjectMapper();
+        // objectMapper.writeValue(new File("src/unsw/gloriaromanus/testFile.json"), unitStats.toString());
+
+        FileWriter file = new FileWriter("src/unsw/gloriaromanus/testFile.json");
+        file.write(unitStats.toString());
+        file.close();
+    }*/
 }
