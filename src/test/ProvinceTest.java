b72bccb57da4f0619981cf922a264c5516655fab
Julie <julie.seo.2000@gmail.com>
2020-11-01 14:08:33 +1100

    Removed the trailing new lines.

--- src/test/ProvinceTest.java
+++ src/test/ProvinceTest.java
@@ -13,12 +13,10 @@ import unsw.gloriaromanus.Province;
 public class ProvinceTest {
     @Test
     private void TrainingCostTest() throws IOException {
-        Player pl = new Player("faction");
+        Player pl = new Player(0, "faction");
         String unitConfig = (Files.readString(Paths.get("src/unsw/gloriaromanus/unit_config.json")));
         Province pr = new Province("province", pl, unitConfig);
         pr.trainUnit("legionary", 100);
         assert(pl.getGold() == 99000);
     }
-    
-    
 }
