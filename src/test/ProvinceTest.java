acb868278bd25651ff3b2286e23e0b9dea8e5137
Julie <julie.seo.2000@gmail.com>
2020-11-01 12:25:12 +1100

    Added tests for training costs of soldiers in a province.

---
+++ src/test/ProvinceTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import unsw.gloriaromanus.Player;
+import unsw.gloriaromanus.Province;
+
+public class ProvinceTest {
+    @Test
+    private void TrainingCostTest() throws IOException {
+        Player pl = new Player("faction");
+        String unitConfig = (Files.readString(Paths.get("src/unsw/gloriaromanus/unit_config.json")));
+        Province pr = new Province("province", pl, unitConfig);
+        pr.trainUnit("legionary", 100);
+        assert(pl.getGold() == 99000);
+    }
+    
+    
+}
